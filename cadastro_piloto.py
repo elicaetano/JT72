9674dd9e187027fe25317fa00913e1bd320ef6c0
elicaetano <100229725+elicaetano@users.noreply.github.com>
2022-10-18 11:02:00 -0300

    Update cadastro_piloto.py

--- cadastro_piloto.py
+++ cadastro_piloto.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
-app.config['MYSQL_DATABASE_DB'] = 'jt72'
+app.config['MYSQL_DATABASE_DB'] = 'teste'
 app.config['MYSQL_DATABASE_HOST'] = 'db'
 mysql.init_app(app)
 
